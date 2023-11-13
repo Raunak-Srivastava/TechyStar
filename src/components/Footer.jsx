cda4cb3223df4ecf48fef4228026111a849abd58
Raunak-Srivastava <2021ugee081@nitjsr.ac.in>
2023-11-13 18:55:19 +0530

    React Project1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://www.youtube.com/channel/UCuGVslnx8374NSdiVNiuUYQ">YouTube</a>
+                <a href="https://www.linkedin.com/in/raunak-srivastava6762/">LinkedIn</a>
+                <a href="https://github.com/Raunak-Srivastava">GitHub</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
